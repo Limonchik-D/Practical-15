42501e1482eca5adea8f0da674c810289726de39
Limonchik-D <DavLimon4@yandex.com>
2024-12-18 22:11:28 +0200

    Add files via upload

---
+++ Task 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {/// <summary>
+     /// Задача 2. Заполнить массив из восьми элементов следующими значениями с использованием явной инициализации:
+     /// первый элемент массива равен 37, второй — 0, третий — 50, четвертый — 46, пятый — 34, шестой — 46, седьмой — 0, 
+     /// восьмой —13. Найти максимальный и минимальный элементы массива.
+     /// </summary>
+        static void Main(string[] args)
+        {
+
+            int[] array = { 37, 0, 50, 46, 34, 46, 0, 13 };
+
+            int max = array.Max();
+            int min = array.Min();
+            Console.WriteLine($"Минимальное значенеи массива: {min}, махсимальное значение: {max}. ");
+            Console.ReadKey();
+        }
+    }
+}
